c0c2f5011cd4802a6cbb2bb000a1f89909ed9481
Idris Dopico Peña <idris.dopico@usmedia.nl>
2018-03-08 08:40:28 +0100

    first commit

---
+++ gulp/_config.js
@@ -0,0 +1,53 @@
+const paths = {
+    src: {
+        src: "./src/",
+        twig: "./src/twig/",
+        scripts: "./src/js/",
+        styles: "./src/sass/",
+        favicons: "./src/static/favicons/",
+        icons: "./src/icons/",
+        static: "./src/static/",
+        vendor: "./node_modules/"
+    },
+
+    target: {
+        dist: {
+            dist: "./dist/",
+            styles: "./dist/assets/css/",
+            scripts: "./dist/assets/js/",
+            static: "./dist/assets/",
+            img: "./dist/assets/img",
+            favicons: "./dist/assets/favicons/"
+        }
+    }
+};
+
+const js = {
+    head: [
+        paths.src.scripts + "./head/*.js"
+    ],
+    app: [
+        paths.src.scripts + "app/components/*.js",
+        paths.src.scripts + "app/widgets/*.js",
+        paths.src.scripts + "app/app.js"
+    ],
+    lib: [
+        paths.src.vendor + "./jquery/dist/jquery.min.js"
+    ]
+};
+
+const clean = [
+    paths.target.dist.dist
+];
+
+const favicon = {
+    name:           "",
+    description:    "",
+    background:     "#FFF",
+    url:            ""
+};
+
+exports.paths   = paths;
+exports.js      = js;
+exports.clean   = clean;
+exports.favicon = favicon;
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp          = require("gulp");
+const sass          = require("./gulp/sass");
+const lint          = require("./gulp/lint");
+const svg           = require("./gulp/svg");
+const copy          = require("./gulp/copy");
+const javascript    = require("./gulp/javascript");
+const twig          = require("./gulp/twig");
+const serve         = require("./gulp/serve");
+const clean         = require("./gulp/clean");
+const favicons      = require("./gulp/favicons");
+
+gulp.task("default", gulp.series(
+    clean.clean,
+
+    gulp.parallel(
+        lint.lintSass
+    ),
+    gulp.parallel(
+        sass.compileSass,
+        javascript.compileHead,
+        javascript.compileApp,
+        javascript.compileLib,
+        svg.generateIcons,
+        copy.copyDist
+    ),
+    gulp.parallel(
+        twig.buildTwig
+    ),
+    serve.serve
+));
+
+gulp.task("build", gulp.series(
+    clean.clean,
+
+    gulp.parallel(
+        sass.compileSass,
+        javascript.compileHead,
+        javascript.compileApp,
+        javascript.compileLib,
+        svg.generateIcons,
+        copy.copyDist
+    ),
+    gulp.parallel(
+        twig.buildTwig
+    )
+));
+
+gulp.task("icons", gulp.parallel(
+    svg.generateIcons
+));
+
+gulp.task("favicons", gulp.parallel(
+    favicons.generateFavicons
+));
+
+gulp.task("clean", gulp.parallel(
+    clean.clean
+));
