3a8fbb46bf755f3c6ae97e19746719017a26a1f2
Idris Dopico Peña <idris.dopico@usmedia.nl>
2018-05-09 11:39:21 +0200

    added mouse animation

--- gulp/fonts.js
+++ gulp/fonts.js
@@ -4,7 +4,7 @@ const gulpFont      = require ("gulp-font");
 const paths         = require("./_config").paths;
 
 function generateFonts() {
-    return gulp.src(paths.src.fonts + '*.woff')
+    return gulp.src(paths.src.fonts + '**')
         .pipe(gulp.dest(paths.target.dist.fonts))
 }
 
--- src/js/app/components/onload.js
+++ src/js/app/components/onload.js
@@ -1,3 +1,4 @@
 window.setTimeout(function () {
     $('.header__content').addClass('is-visible');
+    $('.scroll-btn').addClass('active');
 }, 250);
