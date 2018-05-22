362c8e998fab13d31aedbf2d56428354dc4e714e
Idris Dopico Peña <idopico@zigwebsoftware.nl>
2018-05-22 20:12:02 +0200

    added menu, logo and animations

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,6 +1,19 @@
+const $menu = $('.menu');
+
 $('.scroll-btn').click(function () {
     $("body, html").animate({
         scrollTop: $('#anchor').offset().top
     }, 500);
     return false;
+});
+
+
+$menu.click(function () {
+    if ($menu.hasClass('is-open')) {
+        $(this).removeClass('is-open');
+        $('.menu__logo').removeClass('menu__logo--white');
+    } else {
+        $(this).addClass('is-open');
+        $('.menu__logo').addClass('menu__logo--white');
+    }
 });
