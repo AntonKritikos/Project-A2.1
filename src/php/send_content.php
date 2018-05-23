8338165ddf2260261c01cfe00e6bbd8a8b99dc55
ivorkill <davevanrossum@gmail.com>
2018-05-23 14:42:57 +0200

    Finished Upload

--- src/php/send_content.php
+++ src/php/send_content.php
@@ -1,4 +1,7 @@
 <?php
+echo "TEst";
+require 'config/config.php';
+require 'config/dbconnect.php';
 	$title = $mysqli->real_escape_string($_POST['title']);
 	$title_color = $mysqli->real_escape_string($_POST['title_color']);
 	$subtitle = $mysqli->real_escape_string($_POST['subtitle']);
@@ -12,12 +15,12 @@
 	if(isset($_FILES['image']))
 	{
 	    $errors     = array();
-	    $maxsize    = 1000;
+	    $maxsize    = 100000;
 		$needheight = 800;
 		$needwidth = 800;
 		$file= $_FILES['image']['tmp_name'];
 		$filename= $_FILES['image']['name'];
-		$destination= 'uploads/'.$_FILES['image']['name'];
+		$destination= '../static/img/'.$_FILES['image']['name'];
 
 		if(move_uploaded_file($file, $destination))
 		{
@@ -34,26 +37,33 @@
 				$errors[] = 'Verkeerde dimensies gebruikt. Het bestand mag maximaal 800 pixels hoog zijn en 800 pixels breed.';
 			}
 
-		    if(count($errors) === 0) {
-				$sql = ("INSERT INTO layout (title, title_color, subtitle, subtitle_color, content, background_color, background_image, button_text, button_link, button_align) VALUES ('$title', '$title_color', '$subtitle', '$subtitle_color', '$content', '$background_color', '$destination', '$button_text', '$button_link', '$button_align'");
+			var_dump($errors);
+
+		    if(count($errors) === 0)
+		    {
+				$sql = ("INSERT INTO layout (title, title_color, subtitle, subtitle_color, content, background_color, background_image, button_text, button_link, button_align) VALUES ('$title', '$title_color', '$subtitle', '$subtitle_color', '$content', '$background_color', '$destination', '$button_text', '$button_link', '$button_align')");
+				var_dump($sql);
 			    $results = $mysqli->query($sql);
 			    echo "Met succes geupload";
-			    echo "<a href='?action=account'>Ga terug</a>";
+			    echo "<a href='../twig/pages/admin.twig'>Ga terug</a>";
 		    }
 		}
 		else
 		{
 		  echo 'Problemen met het uploaden van: '.$filename."<br>";
-		  echo "<a href='?action=account'>Ga terug</a>";
+		  echo "<a href='../twig/pages/cms-item-1.twig'>Ga terug</a>";
 		}
 	}
 	else
 	{
 		$result = $mysqli->query("INSERT INTO layout (title, title_color, subtitle, subtitle_color, content, background_color, button_text, button_link, button_align) VALUES ('$title', '$title_color', '$content', '$background_color', '$button_text', '$button_link', '$button_align'");
+		var_dump($result);
 
 		?>
 		<div class="succes">
 			<p>Het stuk is succesvol toegevoegd</p>
+			<a href="../twig/pages/admin.twig">Ga terug</a>
 		</div>
+		<?php
 	}
 ?>
