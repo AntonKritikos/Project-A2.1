f0199e5c1881682a9dfe77ff8f47bd7f63fe5ce3
Idris Dopico Peña <idopico@zigwebsoftware.nl>
2018-05-26 15:42:57 +0200

    .

--- src/js/app/components/paralax.js
+++ src/js/app/components/paralax.js
@@ -1,12 +1,12 @@
-const velocity = .5;
-
-function paralax(){
-    let pos = $(window).scrollTop();
-    $('.innovation').each(function() {
-        const $element = $(this);
-        let height = $element.height() -18;
-        $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px');
-    });
-}
-
-$(window).bind('scroll', paralax);
+// const velocity = .5;
+//
+// function paralax(){
+//     let pos = $(window).scrollTop();
+//     $('.innovation').each(function() {
+//         const $element = $(this);
+//         let height = $element.height() -18;
+//         $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px');
+//     });
+// }
+//
+// $(window).bind('scroll', paralax);
