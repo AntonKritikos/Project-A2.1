3418e99f109f217eccb986e418ec7dc678aa2505
Idris Dopico Peña <idopico@zigwebsoftware.nl>
2018-06-11 10:52:17 +0200

    fixed some alignment etc

--- src/js/app/components/paralax.js
+++ src/js/app/components/paralax.js
@@ -2,13 +2,11 @@ const velocity = .15;
 
 function paralax(){
     let pos = $(window).scrollTop();
-    if ($('.innovation').hasClass('.innovation--half')) {
-        $('.innovation__background').each(function() {
-            const $element = $(this);
-            let height = $element.height() -18;
-            $(this).css('backgroundPosition', '45% ' + Math.round((height - pos) * velocity) +  'px');
-        });
-    }
+    $('.innovation__background').each(function() {
+        const $element = $(this);
+        let height = $element.height() -18;
+        $(this).css('backgroundPosition', '45% ' + Math.round((height - pos) * velocity) +  'px');
+    });
 }
 
 if ($(window).width() > '768') {
